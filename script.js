2e6ff3e4550be4dc8c65697cdf0679a9bd78094c
Hanna Bacelar <hannabacelar@hotmail.com>
2021-12-23 15:10:06 -0300

    implementando logica de criaçao das paletas de cores

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-let colunaCapturada= document.querySelector("#pixel-board")
+function criaPaletaDeCores() {
+    let capturaClassColor = document.getElementsByClassName("color")
+
+    for (let i = 0; i < capturaClassColor.length; i += 1) {
+        if (i === 0) {
+            capturaClassColor[i].style.backgroundColor = "black";
+        } else if (i === 1) {
+            capturaClassColor[i].style.backgroundColor = "pink";
+        } else if (i === 2) {
+            capturaClassColor[i].style.backgroundColor = "yellow";
+        } else if (i === 3) {
+            capturaClassColor[i].style.backgroundColor = "blue";
+        }
+
+    }
+}
+criaPaletaDeCores();
+
+
 
 function criaColunas() {
-    for (let i=0; i <= 4; i+=1) {
+    let colunaCapturada = document.querySelector("#pixel-board")
+    for (let i = 0; i <= 4; i += 1) {
         let criaUl = document.createElement("ul")
-        criaUl.className = "coluna" 
+        criaUl.className = "coluna"
         colunaCapturada.appendChild(criaUl)
 
-    for (let i2 =0;i2 <=4; i2 +=1){
-        let criaLi = document.createElement("li")
-        criaLi.className = "pixel"
-        criaUl.appendChild(criaLi)
-    }
+        for (let i2 = 0; i2 <= 4; i2 += 1) {
+            let criaLi = document.createElement("li")
+            criaLi.className = "pixel"
+            criaUl.appendChild(criaLi)
+        }
 
     }
-    
-    
+
+
 }
 criaColunas()
-
 
