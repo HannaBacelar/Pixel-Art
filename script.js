fbb6c650263d192b66fc491df4d57f7fe8dd1741
Hanna Bacelar <hannabacelar@hotmail.com>
2021-12-23 00:56:49 -0300

    refatorando codigo.Criando colunas de forma dinamica e perfomatica

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-let corPreta = document.querySelector("#preta");
-corPreta.classList.add("selected");
+let colunaCapturada= document.querySelector("#pixel-board")
+
+function criaColunas() {
+    for (let i=0; i <= 4; i+=1) {
+        let criaUl = document.createElement("ul")
+        criaUl.className = "coluna" 
+        colunaCapturada.appendChild(criaUl)
+
+    for (let i2 =0;i2 <=4; i2 +=1){
+        let criaLi = document.createElement("li")
+        criaLi.className = "pixel"
+        criaUl.appendChild(criaLi)
+    }
+
+    }
+    
+    
+}
+criaColunas()
 
 
